d3fa5df547ddba1cd8bdea1f757b0ac8aa293870
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-28 23:37:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+ new_numbers = [1, 2, 3]
+  count = 0
+ # numbers.map{ |i| i * i} new_numbers
+ while count < numbers.length do
+    puts numbers[i = (a * a)]
+    new_numbers<< numbers.each
+   count += 1
+  end
+ # new_numbers
 end
