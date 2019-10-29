75e6fb86a16aeaa8bc4387ec957fa8517587eacf
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-29 01:25:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
- new_numbers = []
+ new_array = []
   count = 0
 
  while count < array.length do
-    new_array<< array[count]**2
+    new_array<< array[count] ** 2
    count += 1
   end
  new_array 
